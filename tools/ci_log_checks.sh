8a278cc2a2a78bea4c81f340b859e0f6f4c116a0
Michael Still <mikal@stillhq.com>
2024-12-13 12:45:46 +1100

    Check syslog.1 as well as syslog.

--- tools/ci_log_checks.sh
+++ tools/ci_log_checks.sh
@@ -5,19 +5,25 @@ failures=0
 echo
 echo "Running log checks for branch ${1} and job ${2}."
 echo
-etcd_conns=`grep -c "Building new etcd connection" /var/log/syslog || true`
-echo "This CI run created $etcd_conns etcd connections."
-if [ $etcd_conns -gt 5000 ]; then
-echo "FAILURE: Too many etcd clients!"
-failures=$(( $failures + 1))
+etcd_conns_a=$(grep -c "Building new etcd connection" /var/log/syslog || true)
+etcd_conns_b=$(grep -c "Building new etcd connection" /var/log/syslog.1 || true)
+echo "etcd connections: ${etcd_conns_a} from syslog, ${etcd_conn_b} from syslog.1"
+etcd_conns=$(( ${etcd_conns_a} + ${etcd_conns_b} ))
+echo "This CI run created ${etcd_conns} etcd connections."
+if [ ${etcd_conns} -gt 5000 ]; then
+    echo "FAILURE: Too many etcd clients!"
+    failures=$(( ${failures} + 1 ))
 fi
 
 echo
-sigterms=`grep -c "Sent SIGTERM to " /var/log/syslog || true`
-echo "This CI run sent $sigterms SIGTERM signals while shutting down."
-if [ $sigterms -gt 50 ]; then
-echo "FAILURE: Too many SIGTERMs sent!"
-failures=$(( $failures + 1))
+sigterms_a=$(grep -c "Sent SIGTERM to " /var/log/syslog || true)
+sigterms_b=$(grep -c "Sent SIGTERM to " /var/log/syslog.1 || true)
+echo "sigterms: ${sigterms_a} from syslog, ${sigterms_b} from syslog.1"
+sigtems=$(( ${sigterms_a} + ${sigterms_b} ))
+echo "This CI run sent ${sigterms} SIGTERM signals while shutting down."
+if [ ${sigterms} -gt 50 ]; then
+    echo "FAILURE: Too many SIGTERMs sent!"
+    failures=$(( ${failures} + 1 ))
 fi
 
 # NOTE(mikal): online upgrades are forbidden in these fresh install
@@ -40,8 +46,7 @@ FORBIDDEN=("Traceback (most recent call last):"
            "cluster wide cleanup daemon is deleting this IPAM as leaked"
            "Cleaning up leaked vxlan"
            "invalid salt"
-           "unable to execute QEMU command"
-           "segfault")
+           "unable to execute QEMU command")
 
 if [ $(echo "${1}" | grep -c "v0.7" || true) -lt 1 ]; then
     echo "INFO: Including forbidden strings for v0.8 onwards."
@@ -50,11 +55,10 @@ if [ $(echo "${1}" | grep -c "v0.7" || true) -lt 1 ]; then
     FORBIDDEN+=("WORKER TIMEOUT")
     FORBIDDEN+=("Repeated failures to add address to device")
     FORBIDDEN+=("Lock held by missing process on this node")
+    FORBIDDEN+=("segfault")
+    FORBIDDEN+=("StatusCode.UNAVAILABLE")
+    FORBIDDEN+=("*** Check failure stack trace: ***")
 fi
-
-# I would like to forbid these, but haven't been able to nail down the root
-# cause yet:
-# FORBIDDEN+=("*** Check failure stack trace: ***")
 
 if [ $(echo "${2}" | grep -c "upgrade" || true) -lt 1 ]; then
     echo "INFO: Including forbidden strings for non-upgrade jobs."
@@ -65,12 +69,15 @@ IFS=""
 for forbid in ${FORBIDDEN[*]}
 do
     echo "    Check for >>${forbid}<< in logs."
-    count=$(grep -c -i "$forbid" /var/log/syslog || true)
-    if [ ${count} -gt 0 ]
-    then
-        echo "FAILURE: Forbidden string found in logs ${count} times."
-        failures=$(( $failures + 1))
-    fi
+
+    for target in /var/log/syslog /var/log/syslog.1; do
+        count=$(grep -c -i "$forbid" "${target}" || true)
+        if [ ${count} -gt 0 ]
+        then
+            echo "FAILURE: Forbidden string found in ${target} ${count} times."
+            failures=$(( $failures + 1))
+        fi
+    done
 done
 
 # Forbidden once stable, which we currently define as after the first 1,000
@@ -81,6 +88,7 @@ IFS=""
 for forbid in ${FORBIDDEN_ONCE_STABLE[*]}
 do
     echo "    Check for >>${forbid}<< in stable logs."
+
     count=$(tail -n +1000 /var/log/syslog | grep -c -i "$forbid" || true)
     if [ ${count} -gt 0 ]
     then
