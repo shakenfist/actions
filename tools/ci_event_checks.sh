c0b2a5e0bfec51467cc719a53ce609a40d15b8fc
Michael Still <mikal@stillhq.com>
2024-12-14 16:53:41 +1100

    Additional etcd checks.

--- tools/ci_event_checks.sh
+++ tools/ci_event_checks.sh
@@ -6,15 +6,28 @@ failures=0
 # Ensure we're not smashing etcd too hard.
 revisions=$(etcdctl get / --write-out=json | jq .header.revision)
 echo "Number of etcd revisions in this test run: ${revisions}"
-if [ ${revisions} -gt 175000 ]; then
+if [ ${revisions} -gt 100000 ]; then
     echo "FAILURE: Too many etcd writes!"
     failures=$(( $failures + 1))
 fi
 echo
 
+# Ensure we're not creating heaps of etcd clients.
+etcd_clients_gateway=$(grep -c "Creating new etcd client via gateway" /var/log/syslog)
+echo "Number of etcd3gw clients created: ${etcd_clients_gateway}"
+etcd_clients_native=$(grep -c "Creating new etcd client via native protocol" /var/log/syslog)
+echo "Number of native etcd clients: ${etcd_clients_native}"
+echo
+
+# Ensure we're not failing heaps of etcd requests
+etcd_failures_gateway=$(grep -c "Failed etcd request via gateway" /var/log/syslog)
+echo "Number of etcd3gw failures: ${etcd_failures_gateway}"
+etcd_failures_native=$(grep -c "Failed etcd request via native protocol" /var/log/syslog)
+echo "Number of native etcd failures: ${etcd_failures_native}"
+echo
+
 # Event statistics.
 if [ $(echo ${1} | egrep -c "^0.[1234567]") -eq 0 ]; then
-
     export SHAKENFIST_ETCD_HOST=10.0.0.10
     /srv/shakenfist/venv/bin/python3 tools/event_statistics.py || true
     failures=$(( $failures + $? ))
