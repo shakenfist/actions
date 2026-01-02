c51e6ced25e2d95785945d9d88aaf39a9abcde20
Michael Still <mikal@stillhq.com>
2026-01-02 19:50:34 +1100

    Detect another libvirt failure.

--- tools/ci_log_checks.sh
+++ tools/ci_log_checks.sh
@@ -65,7 +65,8 @@ FORBIDDEN=("Traceback (most recent call last):"
            "Cleaning up leaked vxlan"
            "invalid salt"
            "unable to execute QEMU command"
-           "bad argument type for built-in operation")
+           "bad argument type for built-in operation"
+           "libvirt: QEMU Driver error : unsupported configuration")
 
 if [ $(echo "${1}" | grep -c "v0.7" || true) -lt 1 ]; then
     echo "INFO: Including forbidden strings for v0.8 onwards."
